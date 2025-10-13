6074836a304f1d87c885e7b443b3400399e8639f
Kyrylo <kirill.grig.2000@gmail.com>
2025-10-14 01:02:14 +0300

    Show admin controls for self

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ class EventParticipant(db.Model):
 class GameLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event_name = db.Column(db.String(100), nullable=False)
-    event_date = db.Column(db.String(20), nullable=False)
+    event_date = db.Column(db.String(30), nullable=False)
     logged_at = db.Column(db.DateTime, default=datetime.utcnow)
     active_participants_json = db.Column(db.Text, default='[]')
     cancelled_participants_json = db.Column(db.Text, default='[]')
